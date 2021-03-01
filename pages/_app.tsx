7cdd4a510744522ce17305b533d876053c120fc6
José Bruno <josebrunocampanholi@gmail.com>
2021-03-01 17:10:27 -0300

    Mais uma vez...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../src/styles/Global.css';
 import { ThemeProvider } from 'styled-components';
 import { temaClaro, temaEscuro } from '../src/styles/themes';
-import { TextProvider } from '../src/contexts/TextContext';
 import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
